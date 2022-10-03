4e38559133324750cd5d373b9cd73d405569868e
Naywvi <nagib.lakhdari.pro@gmail.com>
2022-10-03 11:23:12 +0200

    .

---
+++ create-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE students {
+
+    StudentId INTEGER auto-increment and primary key not null,
+    FirstName VARCHAR(80) not null,
+    LastName VARCHAR(80) not null,
+    Birthday DATE  not null,
+    City VARCHAR(50) not null
+
+};
