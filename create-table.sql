b8661dc9d30356cbe58e702d95893f5aa98aed20
Naywvi <nagib.lakhdari.pro@gmail.com>
2022-10-03 11:35:12 +0200

    .

--- create-table.sql
+++ create-table.sql
@@ -1,10 +1,7 @@
-CREATE TABLE students 
-(
-
-    StudentId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,,
+CREATE TABLE students(
+    StudentId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     FirstName CHAR(80) not null,
     LastName CHAR(80) not null,
-    Birthday DATE  not null,
-    City CHAR(50) not null
-
+    Birthday DATE not null,
+    City CHAR(50) not null,
 );
