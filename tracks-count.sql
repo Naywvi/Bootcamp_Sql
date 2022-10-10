867e2de53a534a67a4b825351fb55ef843e79908
Naywvi <nagib.lakhdari.pro@gmail.com>
2022-10-10 14:12:28 +0200

    .

--- tracks-count.sql
+++ tracks-count.sql
@@ -1,4 +1,4 @@
-SELECT genres.Name, COUNT(genres.GenreId) FROM genres
+SELECT genres.Name, COUNT(genres.GenreId) AS NumberOfTracks FROM genres
 JOIN tracks ON tracks.GenreId = genres.GenreId
 GROUP BY tracks.GenreId 
 ORDER BY genres.Name
