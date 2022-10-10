aebcdd60c0db1d8ff24beafab6b326229ad55032
Naywvi <nagib.lakhdari.pro@gmail.com>
2022-10-10 12:54:40 +0200

    .

--- select-artists-condition.sql
+++ select-artists-condition.sql
@@ -2,4 +2,4 @@ SELECT artists.Name FROM artists
 INNER JOIN albums ON artists.ArtistId = albums.ArtistId
 GROUP BY (artists.Name) 
 HAVING count(albums.ArtistId) >= 4
-ORDER BY artists.Name desc DESC
+ORDER BY artists.Name DESC
