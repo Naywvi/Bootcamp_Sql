72d6aa33574cd0809ace734cda906f586e88cee4
Naywvi <nagib.lakhdari.pro@gmail.com>
2022-10-10 13:09:53 +0200

    .

--- select-customers-upper.sql
+++ select-customers-upper.sql
@@ -1,3 +1,3 @@
 SELECT UPPER(customers.LastName) AS LastNameUpper, customers.FirstName FROM customers
 GROUP BY customers.LastName
-HAVING LENGTH(customers.FirstName) > 5
+HAVING LENGTH(customers.FirstName) > 5 and LENGTH(customers.LastName) > 5
