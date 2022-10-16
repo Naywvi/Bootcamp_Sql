18707bec1a26b4ec3813b3fdcf06b73d27cea52f
Naywvi <nagib.lakhdari.pro@gmail.com>
2022-10-16 17:10:06 +0200

    x

--- third-best-seller.sql
+++ third-best-seller.sql
@@ -4,4 +4,5 @@ WHEN count(customers.FirstName) = 18 THEN emp.LastName || ' ' || emp.FirstName
 END as '3rd best seller'
 FROM employees as emp
 INNER JOIN customers ON customers.SupportRepId = emp.EmployeeId
+where emp.BirthDate = '1965-03-03 00:00:00'
 GROUP BY emp.LastName
