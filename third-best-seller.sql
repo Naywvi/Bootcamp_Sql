a9d96e88f61a256ea372938eec92d781e4db5a2e
Naywvi <nagib.lakhdari.pro@gmail.com>
2022-10-16 17:18:08 +0200

    x

--- third-best-seller.sql
+++ third-best-seller.sql
@@ -1,8 +1,11 @@
 SELECT
 CASE
 WHEN count(customers.FirstName) = 18 THEN emp.LastName || ' ' || emp.FirstName 
-WHEN count(customers.FirstName) IS NULL THEN 'coucou'
+
 END as '3rd best seller'
 FROM employees as emp
 INNER JOIN customers ON customers.SupportRepId = emp.EmployeeId
+
 GROUP BY emp.LastName
+
+LIMIT 1
