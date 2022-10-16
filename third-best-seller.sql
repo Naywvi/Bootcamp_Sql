b605e6d17732f4a6f15d3a238e7e3cbf3505c271
Naywvi <nagib.lakhdari.pro@gmail.com>
2022-10-16 17:04:42 +0200

    x

---
+++ third-best-seller.sql
@@ -0,0 +1,7 @@
+SELECT
+CASE
+WHEN count(customers.FirstName) = 18 THEN emp.LastName || ' ' || emp.FirstName
+END as '3rd best seller'
+FROM employees as emp
+INNER JOIN customers ON customers.SupportRepId = emp.EmployeeId
+WHERE customers.SupportRepId = 5
