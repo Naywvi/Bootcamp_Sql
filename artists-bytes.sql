3dbbdd70f536b1295d85b893132db2f4d00879a8
Naywvi <nagib.lakhdari.pro@gmail.com>
2022-10-16 15:18:10 +0200

    x

--- artists-bytes.sql
+++ artists-bytes.sql
@@ -1,3 +1,4 @@
-SELECT tracks.Composer as ArtistName, albums.Title as AlbumName, tracks.Name as TrackName, ROUND(ROUND(tracks.Bytes/1000)/1000,2) || ' MB' as MegaBytes FROM tracks
+SELECT artists.Name as ArtistName, albums.Title as AlbumName, tracks.Name as TrackName, ROUND(ROUND(tracks.Bytes/1000)/1000,2) || ' MB' as MegaBytes FROM tracks
 INNER JOIN albums ON albums.AlbumId = tracks.AlbumId
+INNER JOIN artists ON albums.ArtistId = artists.ArtistId
 WHERE tracks.AlbumId = 89
