2ee4e37c8f5aa4be97666f33e099b118e27434cb
Naywvi <nagib.lakhdari.pro@gmail.com>
2022-10-18 11:29:47 +0200

    x

--- average-invoices-tracks.sql
+++ average-invoices-tracks.sql
@@ -1,6 +1,6 @@
-SELECT i.InvoiceId, ROUND(AVG(i."UnitPrice" * i.Quantity), 2) AS 'Average Price', 
-    CAST(SUM(t."Milliseconds")/1000 AS INT) AS 'Track Total Time',
-    ROUND(AVG(i."UnitPrice" * i.Quantity) / (SUM(t."Milliseconds")/1000), 5) || '€' AS 'Price by second'
+SELECT i.InvoiceId, ROUND(AVG(i.UnitPrice * i.Quantity), 2) AS 'Average Price', 
+    CAST(SUM(t.Milliseconds)/1000 AS INT) AS 'Track Total Time',
+    ROUND(AVG(i."UnitPrice" * i.Quantity) / (SUM(t.Milliseconds)/1000), 5) || '€' AS 'Price by second'
 FROM invoice_items i
-INNER JOIN tracks t ON t."TrackId" = i."TrackId"
-GROUP BY i."InvoiceId
+INNER JOIN tracks t ON t.TrackId = i.TrackId
+GROUP BY i.InvoiceId
