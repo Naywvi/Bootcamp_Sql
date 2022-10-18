496de2af91d41b92310a01a33577082115f4a530
Naywvi <nagib.lakhdari.pro@gmail.com>
2022-10-18 11:01:17 +0200

    x

--- -- SQLite.sql
+++
@@ -1,8 +0,0 @@
--- SQLite
-SELECT DISTINCT InvoiceId FROM invoice_items 
-WHERE TrackId IN (
-SELECT TrackId FROM tracks 
-WHERE Milliseconds IN (
-SELECT MAX(Milliseconds) FROM tracks
-GROUP BY GenreId))
-ORDER BY InvoiceId
