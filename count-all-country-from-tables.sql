18aaad594d78a18410be947d006c7646a2a53286
Naywvi <nagib.lakhdari.pro@gmail.com>
2022-10-18 19:59:55 +0200

    x

--- count-all-country-from-tables.sql
+++ count-all-country-from-tables.sql
@@ -1,79 +1,25 @@
-SELECT 
-c1
-
-
-
-
-from
-(select country as c1, CASE
-
-WHEN country = cemp THEN tot4
-else 0
-END f1
-FROM
-(
-    SELECT country, tot  FROM 
-        (SELECT Country, COUNT() tot FROM Employees
-        group by country
-        UNION all
-        SELECT Country, COUNT() tot FROM Customers
-        group by country
-        UNION all
-        SELECT BillingCountry AS Country, COUNT(*) FROM Invoices
-        group by country)
-)a,
-(
-    SELECT cinv as cemp, tot4  FROM 
-        (SELECT country as cinv, COUNT() tot4 FROM employees
-        group by cinv)
-)
-),
-
-(select country as c2, CASE
-
-WHEN country = cinv THEN tot4
-else 0
-END f2
-FROM
-(
-    SELECT country, tot  FROM 
-        (SELECT Country, COUNT() tot FROM Employees
-        group by country
-        UNION all
-        SELECT Country, COUNT() tot FROM Customers
-        group by country
-        UNION all
-        SELECT BillingCountry AS Country, COUNT(*) FROM Invoices
-        group by country)
-),
-(
-    SELECT cinv, tot4  FROM 
-        (SELECT BillingCountry as cinv, COUNT() tot4 FROM invoices
-        group by cinv)
-)
-),
-
-(select country as c3, CASE
-
-WHEN country = cust THEN tot4
-else 0
-END f3
-FROM
-(
-    SELECT country, tot  FROM 
-        (SELECT Country, COUNT() tot FROM Employees
-        group by country
-        UNION all
-        SELECT Country, COUNT() tot FROM Customers
-        group by country
-        UNION all
-        SELECT BillingCountry AS Country, COUNT(*) FROM Invoices
-        group by country)
-),
-(
-    SELECT cinv AS cust, tot4  FROM 
-        (SELECT country as cinv, COUNT() tot4 FROM customers
-        group by cinv)
-)
-)
-
+SELECT tem.Country, COUNT(*) AS 'Total', 
+    IFNULL((
+    SELECT COUNT(*) FROM employees
+    WHERE "Country" = tem.Country
+    GROUP BY "Country"
+    ), 0) AS 'Employees',
+    IFNULL((
+    SELECT COUNT(*) FROM customers
+    WHERE "Country" = tem.Country
+    GROUP BY "Country"
+    ), 0) AS 'Customers',
+    IFNULL((
+    SELECT COUNT(*) FROM invoices
+    WHERE "BillingCountry" = tem.Country
+    GROUP BY "BillingCountry"
+    ), 0) AS 'Invoices'
+FROM (
+  SELECT "Country" FROM employees
+  UNION ALL
+  SELECT "Country" FROM customers
+  UNION ALL
+  SELECT "BillingCountry" FROM invoices
+) AS tem
+GROUP BY Country
+ORDER BY Country
