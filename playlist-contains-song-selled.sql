dfdaaba9eac523d967cbe0e4a26a15f3e667560d
Naywvi <nagib.lakhdari.pro@gmail.com>
2022-10-21 15:25:31 +0200

    x

--- playlist-contains-song-selled.sql
+++ playlist-contains-song-selled.sql
@@ -2,8 +2,17 @@ SELECT p.PlaylistId, p.Name,
 IFNULL(
 ROUND(
 (
-            (SELECT COUNT(TrackId) FROM playlist_track WHERE TrackId IN 
-                (SELECT TrackId FROM invoice_items GROUP BY TrackId HAVING COUNT(TrackId) = 2) AND PlaylistId = p.PlaylistId GROUP BY PlaylistId) /(COUNT(pt.TrackId)
+            (SELECT COUNT(TrackId) FROM playlist_track
+                WHERE TrackId IN 
+                    (
+                        SELECT TrackId FROM invoice_items 
+                        GROUP BY TrackId 
+                        HAVING COUNT(TrackId) = 2
+                    ) 
+                AND PlaylistId = p.PlaylistId 
+                GROUP BY PlaylistId
+            ) 
+                /(COUNT(pt.TrackId)
             * 1.0) 
         * 100)
     , 4)
