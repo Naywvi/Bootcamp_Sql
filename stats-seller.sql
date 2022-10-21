07344bbffd8c76d9a905f4488fa8c20eff770745
Naywvi <nagib.lakhdari.pro@gmail.com>
2022-10-21 15:54:58 +0200

    x

--- stats-seller.sql
+++ stats-seller.sql
@@ -1,13 +1,13 @@
 SELECT e.LastName, e.FirstName,
 IFNULL(
     (
-        SELECT COUNT(*) FROM invoices as i
+        SELECT count(*) FROM invoices as i
     ),0
-) as 'Total sell'
+) as 'Total Sell'
 ,
 IFNULL(
     (
-        SELECT CAST(count(*) as VARCHAR(3)) FROM customers as c
+        SELECT count(*) FROM customers as c
         INNER JOIN invoices as i ON i.CustomerId = c.CustomerId
         WHERE c.SupportRepId = e.EmployeeId
         
@@ -65,11 +65,11 @@ REPLACE(
 REPLACE(
     IFNULL(
         (
-            SELECT CAST(ROUND(ROUND(count(*))*100/146,2) as float) as totall FROM customers as c
+            SELECT ROUND(ROUND(count(*))*100/146,2) as totall FROM customers as c
             INNER JOIN invoices as i ON i.CustomerId = c.CustomerId
             WHERE c.SupportRepId = e.EmployeeId and e.FirstName != 'Jane'
             LIMIT 1
         ),0
     ) 
-,'0','-') as 'Percentage sales compared best seller'
+,'0.0','-') as 'Percentage sales compared best seller'
 FROM employees as e 
