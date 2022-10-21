5bce9599415046f07c0cdd314db08b9f56cdaef5
Naywvi <nagib.lakhdari.pro@gmail.com>
2022-10-21 15:37:13 +0200

    x

--- stats-seller.sql
+++ stats-seller.sql
@@ -1,4 +1,5 @@
 -- SQLite
+
 SELECT e.LastName, e.FirstName,
 IFNULL(
     (
@@ -15,39 +16,45 @@ IFNULL(
     ),0
 ) as 'Total Sell By Employee'
 ,
-IFNULL(
-    (
-        SELECT c.Country FROM customers as c
-        INNER JOIN invoices as i ON i.CustomerId = c.CustomerId
-        WHERE c.SupportRepId = e.EmployeeId 
-        GROUP by c.Country
-        ORDER BY COUNT(*) DESC
-        LIMIT 1
-        
-    ),0
-) as 'Country With Most Sales'
+REPLACE(
+    IFNULL(
+        (
+            SELECT c.Country FROM customers as c
+            INNER JOIN invoices as i ON i.CustomerId = c.CustomerId
+            WHERE c.SupportRepId = e.EmployeeId 
+            GROUP by c.Country
+            ORDER BY COUNT(*) DESC
+            LIMIT 1
+            
+        ),0
+    ) 
+,'0','-')as 'Country With Most Sales'
 ,
-IFNULL(
-    (
-        SELECT g.Name FROM customers as c
-        INNER JOIN invoices as i ON i.CustomerId = c.CustomerId
-        INNER JOIN invoice_items as it ON i.InvoiceId = it.InvoiceId
-        INNER JOIN tracks as t ON it.TrackId = t.TrackId
-        INNER JOIN genres as g ON t.GenreId = g.GenreId
-        WHERE c.SupportRepId = e.EmployeeId 
-        GROUP by g.Name
-        ORDER BY COUNT(*) DESC
-        LIMIT 1
-        
-    ),0
-) as 'Most Genre Selled'
+REPLACE(
+    IFNULL(
+        (
+            SELECT g.Name FROM customers as c
+            INNER JOIN invoices as i ON i.CustomerId = c.CustomerId
+            INNER JOIN invoice_items as it ON i.InvoiceId = it.InvoiceId
+            INNER JOIN tracks as t ON it.TrackId = t.TrackId
+            INNER JOIN genres as g ON t.GenreId = g.GenreId
+            WHERE c.SupportRepId = e.EmployeeId 
+            GROUP by g.Name
+            ORDER BY COUNT(*) DESC
+            LIMIT 1
+            
+        ),0
+    ) 
+,'0','-')as 'Most Genre Selled'
 ,
-IFNULL(
-    (
-        SELECT ROUND(ROUND(count(*))*100/146,2) as totall FROM customers as c
-        INNER JOIN invoices as i ON i.CustomerId = c.CustomerId
-        WHERE c.SupportRepId = e.EmployeeId and e.FirstName != 'Jane'
-        LIMIT 1
-    ),0
-) as 'Percentage sales compared best seller'
+REPLACE(
+    IFNULL(
+        (
+            SELECT ROUND(ROUND(count(*))*100/146,2) as totall FROM customers as c
+            INNER JOIN invoices as i ON i.CustomerId = c.CustomerId
+            WHERE c.SupportRepId = e.EmployeeId and e.FirstName != 'Jane'
+            LIMIT 1
+        ),0
+    ) 
+,'0.0','-') as 'Percentage sales compared best seller'
 FROM employees as e 
